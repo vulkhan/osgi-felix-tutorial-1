1b5ccb91385e248f994e90215a463c35e4a9ae87
Laszlo Pogany <laszlo.pogany@gmail.com>
2016-03-17 20:31:12 +0100

    OSGi tutorial 1 added

---
+++ commands.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+# Modify this line; give the location of the Apache Felix implementation
+LIB_PATH=/c/LIBS/org.apache.felix.main.distribution-5.4.0/felix-framework-5.4.0
+
+
+# Do not modify from here
+mkdir ./classes
+
+
+# Tutorial 1
+javac -d ./classes/ tutorial_example1/*.java -cp $LIB_PATH/bin/felix.jar
+jar cfmv example1.jar tutorial_example1/manifest.mf -C ./classes tutorial/example1
+
+
